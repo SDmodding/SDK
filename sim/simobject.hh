43c28031d3ce462eec000400a8255b9e713704a0
sneakyevil <sneakyevildogs@gmail.com>
2024-10-20 16:21:46 +0200

    Update simobject.hh

--- sim/simobject.hh
+++ sim/simobject.hh
@@ -2,7 +2,13 @@
 
 namespace UFG
 {
+	class SceneObjectProperties;
 	class SimObject;
+	class TransformNodeComponent;
+
+	//-------------------------------------------------------------------
+	// Component
+	//-------------------------------------------------------------------
 
 	class SimComponent : public qSafePointerNode<SimComponent>
 	{
@@ -15,4 +21,32 @@ namespace UFG
 		qList<RebindingComponentHandleBase> m_BoundComponentHandles;
 	};
 	SDK_ASSERT_SIZEOF(SimComponent, 0x40);
+
+	class SimComponentHolder
+	{
+	public:
+		SimComponent* m_pComponent;
+		u32 m_TypeUID;
+	};
+
+	//-------------------------------------------------------------------
+	// Object
+	//-------------------------------------------------------------------
+
+	class SimObject : public qSafePointerNodeWithCallbacks<SimObject>, public qNodeRB<SimObject>
+	{
+	public:
+		qSymbol m_Name;
+		unsigned __int16 m_Flags;
+		char m_ReservedComponentSlots;
+		char m_ResolveRefCount;
+		SceneObjectProperties* m_pSceneObj;
+		TransformNodeComponent* m_pTransformNodeComponent;
+		qArray<SimComponentHolder> m_Components;
+		qList<RebindingComponentHandleBase> m_UnboundComponentHandles;
+
+		virtual void Attach(SimComponent* component, u32 index) = 0;
+		virtual void Detach(SimComponent* component) = 0;
+	};
+	SDK_ASSERT_SIZEOF(SimObject, 0x80);
 }
