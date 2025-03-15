a7b6ae9ec90a2461ab5d75b8b11968bb7a6b27c5
sneakyevil <admin@sneakyevil.eu>
2025-03-15 16:39:17 +0100

    [Impl] qPropertyList Getters & qColour functions

--- propertyset/qpropertylist.hh
+++ propertyset/qpropertylist.hh
@@ -21,6 +21,11 @@ namespace UFG
 
 		SDK_SINLINE qPropertyList* Create(const char* dbg_tag = SDK_VAR(const char*, 0x16BE9F0)) { return SDK_CALL_FUNC(qPropertyList*, 0x1F4060, const char*)(dbg_tag); }
 
+		/* Functions */
+
+		u32 GetRandomIndex() { return SDK_CALL_FUNC(u32, 0x1F8010, void*)(this); }
+		void* GetValuePtr(u32 type_uid, u32 index) { return SDK_CALL_FUNC(void*, 0x1F8920, void*, u32, u32)(this, type_uid, index); }
+		bool GetVariant(u32 index, qPropertySetVariant& value) { return SDK_CALL_FUNC(bool, 0x1F8E90, void*, u32, qPropertySetVariant&)(this, index, value); }
 
 		/* Add Functions */
 
@@ -89,6 +94,74 @@ namespace UFG
 
 		template<>
 		void Add(u16 v) { SDK_CALL_FUNC(void, 0x1E8C10, void*, u16)(this, v); }
+
+		/* Get Functions */
+
+		template <typename T>
+		T* Get(u32 index);
+
+		template<>
+		qMatrix44* Get(u32 index) { return SDK_CALL_FUNC(qMatrix44*, 0x1E9D20, void*, u32)(this, index); }
+
+		template<>
+		qPropertyList* Get(u32 index) { return SDK_CALL_FUNC(qPropertyList*, 0x1E9DE0, void*, u32)(this, index); }
+
+		template<>
+		qPropertySet* Get(u32 index) { return SDK_CALL_FUNC(qPropertySet*, 0x642D0, void*, u32)(this, index); }
+
+		template<>
+		qSymbol* Get(u32 index) { return SDK_CALL_FUNC(qSymbol*, 0x1E9F80, void*, u32)(this, index); }
+
+		template<>
+		qSymbolUC* Get(u32 index) { return SDK_CALL_FUNC(qSymbolUC*, 0x1EA040, void*, u32)(this, index); }
+
+		template<>
+		qVector2* Get(u32 index) { return SDK_CALL_FUNC(qVector2*, 0x1EA100, void*, u32)(this, index); }
+
+		template<>
+		qVector3* Get(u32 index) { return SDK_CALL_FUNC(qVector3*, 0x1EA1C0, void*, u32)(this, index); }
+
+		template<>
+		qVector4* Get(u32 index) { return SDK_CALL_FUNC(qVector4*, 0x1EA280, void*, u32)(this, index); }
+
+		template<>
+		qWiseSymbol* Get(u32 index) { return SDK_CALL_FUNC(qWiseSymbol*, 0x1EA340, void*, u32)(this, index); }
+
+		template<>
+		i64* Get(u32 index) { return SDK_CALL_FUNC(i64*, 0x1EA400, void*, u32)(this, index); }
+
+		template<>
+		__m128* Get(u32 index) { return SDK_CALL_FUNC(__m128*, 0x1E9BC0, void*, u32)(this, index); }
+
+		template<>
+		bool* Get(u32 index) { return SDK_CALL_FUNC(bool*, 0x1EA580, void*, u32)(this, index); }
+
+		template<>
+		const char* Get(u32 index) { return SDK_CALL_FUNC(const char*, 0x1E9AB0, void*, u32)(this, index); }
+
+		template<>
+		f32* Get(u32 index) { return SDK_CALL_FUNC(f32*, 0x1E99F0, void*, u32)(this, index); }
+
+		template<>
+		int* Get(u32 index) { return SDK_CALL_FUNC(int*, 0x1E9870, void*, u32)(this, index); }
+
+		template<>
+		short* Get(u32 index) { return SDK_CALL_FUNC(short*, 0x1E96F0, void*, u32)(this, index); }
+
+		template<>
+		s8* Get(u32 index) { return SDK_CALL_FUNC(s8*, 0x1E9580, void*, u32)(this, index); }
+
+		template<>
+		u8* Get(u32 index) { return SDK_CALL_FUNC(u8*, 0x1E9630, void*, u32)(this, index); }
+
+		template<>
+		u32* Get(u32 index) { return SDK_CALL_FUNC(u32*, 0x1E9930, void*, u32)(this, index); }
+
+		template<>
+		u64* Get(u32 index) { return SDK_CALL_FUNC(u64*, 0x1EA4C0, void*, u32)(this, index); }
+
+		template<>
+		u16* Get(u32 index) { return SDK_CALL_FUNC(u16*, 0x1E97B0, void*, u32)(this, index); }
 	};
 	SDK_ASSERT_SIZEOF(qPropertyList, 0x30);
 }
--- quark/colour.hh
+++ quark/colour.hh
@@ -11,4 +11,9 @@ namespace UFG
 		SDK_INLINE qColour(f32 fR, f32 fG, f32 fB, f32 fA = 1.f) : r(fR), g(fG), b(fB), a(fA) {}
 		SDK_INLINE qColour(int r, int g, int b, int a = 255) : qColour(static_cast<f32>(r) * (1.f / 255.f), static_cast<f32>(g) * (1.f / 255.f), static_cast<f32>(b) * (1.f / 255.f), static_cast<f32>(a) * (1.f / 255.f)) {}
 	};
+
+	/* Static Functions */
+
+	SDK_SINLINE void qColourRGBToHSV(qColour& hsv, const qColour& rgb) { SDK_CALL_FUNC(void, 0x182630, qColour&, const qColour&)(hsv, rgb); }
+	SDK_SINLINE void qColourHSVToRGB(qColour& rgb, const qColour& hsv) { SDK_CALL_FUNC(void, 0x1824E0, qColour&, const qColour&)(rgb, hsv); }
 }
