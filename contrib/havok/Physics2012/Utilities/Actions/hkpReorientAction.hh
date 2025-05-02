9bd1ea3a30b22351de6d9b2806d73eaa6ca1cc79
sneakyevil <admin@sneakyevil.eu>
2025-05-02 13:37:44 +0200

    Update hkpReorientAction.hh

--- contrib/havok/Physics2012/Utilities/Actions/hkpReorientAction.hh
+++ contrib/havok/Physics2012/Utilities/Actions/hkpReorientAction.hh
@@ -10,8 +10,8 @@ public:
 
 	/* Constructors */
 
-	hkpReorientAction() { SDK_CALL_FUNC(void, 0xE12FE0, void*)(this); }
-	hkpReorientAction(hkpRigidBody* body, const hkVector4f& rotationAxis, const hkVector4f& upAxis, f32 strength = 1.f, f32 damping = 0.1f) {
+	void ctor() { SDK_CALL_FUNC(void, 0xE12FE0, void*)(this); }
+	void ctor(hkpRigidBody* body, const hkVector4f& rotationAxis, const hkVector4f& upAxis, f32 strength = 1.f, f32 damping = 0.1f) {
 		SDK_CALL_FUNC(void, 0xE13020, void*, hkpRigidBody*, const hkVector4f&, const hkVector4f&, f32, f32)(this, body, rotationAxis, upAxis, strength, damping);
 	}
 };
