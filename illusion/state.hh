cb350d0a33a30584fd1fcb7c4469a5c0783e608b
sneakyevil <admin@sneakyevil.eu>
2026-04-17 08:12:11 +0200

    Add Illusion::StateSystem

--- illusion/state.hh
+++ illusion/state.hh
@@ -31,4 +31,26 @@ namespace Illusion
 		u32 mNameUID;
 		u32 mParamIndex;
 	};
+
+	class IStateSystem
+	{
+	public:
+	};
+
+	class StateSystem : public IStateSystem
+	{
+	public:
+		StateParamDesc mStateParamDescs[128];
+		StateParamLookup mStateParamLookup[128];
+		int mCurrentParamIndex;
+
+		/* Static Functions */
+
+		SDK_SINLINE StateSystem* Instance() { return SDK_VAR(StateSystem*, 0x2167B40); }
+
+		/* Functions */
+
+		u32 GetParam(u32 uid) { return SDK_CALL_FUNC(u32, 0x923B0, void*, u32)(this, uid); }
+		u32 GetParam(const char* name) { return SDK_CALL_FUNC(u32, 0x92420, void*, const char*)(this, name); }
+	};
 }
