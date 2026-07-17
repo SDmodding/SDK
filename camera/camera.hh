bc84056c3cf00e46d5fe273f6ac3202dc3f9dc5e
sneakyevil <admin@sneakyevil.eu>
2026-07-17 20:12:59 +0200

    Update camera.hh

--- camera/camera.hh
+++ camera/camera.hh
@@ -27,11 +27,23 @@ namespace UFG
 		f32 GetFarPlane() { return SDK_CALL_FUNC(f32, 0x318C0, void*)(this); }
 		f32 GetNearPlane() { return SDK_CALL_FUNC(f32, 0x4FBC0, void*)(this); }
 		void ApplyDutch(f32 radians) { SDK_CALL_FUNC(void, 0xB99F0, void*, f32)(this, radians); }
-		qVector3 GetScreenCoord(const qVector3& world_pos, const qVector2& viewport_scale, bool* is_on_screen = 0) { return SDK_CALL_FUNC(qVector3, 0xB9B70, void*, const qVector3&, const qVector2&, bool*)(this, world_pos, viewport_scale, is_on_screen); }
+
+		qVector3 GetScreenCoord(const qVector3& world_pos, const qVector2& viewport_scale, bool* is_on_screen = 0) 
+		{
+			qVector3 res;
+			return *SDK_CALL_FUNC(qVector3*, 0xB9B70, void*, qVector3*, const qVector3&, const qVector2&, bool*)(this, &res, world_pos, viewport_scale, is_on_screen);
+		}
+
 		const qMatrix44& GetViewProjection() { return SDK_CALL_FUNC(const qMatrix44&, 0xB9C50, void*)(this); }
 		const qMatrix44& GetViewWorld() { return SDK_CALL_FUNC(const qMatrix44&, 0xB9C70, void*)(this); }
 		const qMatrix44& GetWorldView() { return SDK_CALL_FUNC(const qMatrix44&, 0xB9C90, void*)(this); }
-		qVector3 ScreenToWorldRay(const qVector2& screen_pos, const qVector2& viewport_scale) { return SDK_CALL_FUNC(qVector3, 0xBA0B0, void*, const qVector2&, const qVector2&)(this, screen_pos, viewport_scale); }
+
+		qVector3 ScreenToWorldRay(const qVector2& screen_pos, const qVector2& viewport_scale)
+		{
+			qVector3 res;
+			return *SDK_CALL_FUNC(qVector3*, 0xBA0B0, void*, qVector3*, const qVector2&, const qVector2&)(this, &res, screen_pos, viewport_scale);
+		}
+		
 		void SetFov(f32 fov) { SDK_CALL_FUNC(void, 0xBA5E0, void*, f32)(this, fov); }
 		void SetLookAtMatrix(const qVector3& eye, const qVector3& look, const qVector3& up) { SDK_CALL_FUNC(void, 0xBA680, void*, const qVector3&, const qVector3&, const qVector3&)(this, eye, look, up); }
 		void SetViewProjection(f32 fov, f32 aspect, f32 nearPlane, f32 farPlane, f32 xOffset, f32 yOffset, bool applyFovOffset) { SDK_CALL_FUNC(void, 0xBA6E0, void*, f32, f32, f32, f32, f32, f32, bool)(this, fov, aspect, nearPlane, farPlane, xOffset, yOffset, applyFovOffset); }
