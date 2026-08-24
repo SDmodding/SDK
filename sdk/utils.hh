078da90f0e5b04b2d481fbb5b13f41590106269a
sneakyevil <admin@sneakyevil.eu>
2026-08-24 10:54:37 +0200

    Update utils.hh

--- sdk/utils.hh
+++ sdk/utils.hh
@@ -9,3 +9,7 @@ namespace SDK
 		return *reinterpret_cast<u32*>(SDK_RVA(0x201D59C)) == 0x201E094; 
 	}
 }
+
+#ifdef D3D_COMPILER_VERSION
+	#define SDK_D3DCompile() reinterpret_cast<decltype(&D3DCompile)>(GetProcAddress(GetModuleHandleA(SDK_VAR(const char*, 0x201CAD2)), "D3DCompile"))
+#endif
